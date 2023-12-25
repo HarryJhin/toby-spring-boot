53e2f96fcc6deccd60e1a792ea797eda802ddba5
주진현 <joojinhyun00@gmail.com>
2023-12-25 13:45:49 +0900

    refactor: SpringBootApplication 동작과 무관하게 서블릿 컨테이너 시작하기

--- src/main/java/blog/harryjhin/demo/TobySpringBootApplication.java
+++ src/main/java/blog/harryjhin/demo/TobySpringBootApplication.java
@@ -2,12 +2,20 @@ package blog.harryjhin.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
 
-@SpringBootApplication
+// @SpringBootApplication
 public class TobySpringBootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TobySpringBootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        // original
+        // SpringApplication.run(TobySpringBootApplication.class, args);
+
+        // tomcat servlet container
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        WebServer webServer = factory.getWebServer();
+        webServer.start();
+    }
 
 }
